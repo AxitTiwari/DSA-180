a9de58f86e845db6cc5e3b1fd64028f1d5ce69b8
Axit Tiwari <axittiwari0@gmail.com>
2026-03-25 22:15:14 +0530

    Day71: neetcode problem

---
+++ Day71/Neetcode/494.py
@@ -0,0 +1,22 @@
+from typing import List 
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        
+        ## do it again
+
+        dp = {} # (index, total)
+
+        def backtrack(i, total):
+
+            if i == len(nums):
+                return 1 if total == target else 0
+            
+            if (i, total) in dp:
+                return dp[(i, total)]
+
+            dp[(i, total)] = backtrack(i+1, total+nums[i]) + backtrack(i+1, total-nums[i])
+
+            return dp[(i, total)]
+
+        return backtrack(0, 0)
