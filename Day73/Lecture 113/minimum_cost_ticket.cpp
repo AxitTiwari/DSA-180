1e84bbdce4f2c6c1e5073c3bb92234b787e43e20
Axit Tiwari <axittiwari0@gmail.com>
2026-03-27 19:19:44 +0530

    minimum cost for tickets(DP)

---
+++ Day73/Lecture 113/minimum_cost_ticket.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int reccursion(int n, vector<int> &days, vector<int> &cost, int index) {
+
+    if (index >= n) {
+        return 0;
+    }
+
+    int day_pass = cost[0] + reccursion(n, days, cost, index+1);
+
+    // move your index at correct position according to 7 days pass
+    int i;
+    for (i = index; i < n && days[i] < days[index] + 7; i++);
+
+    int week_pass = cost[1] + reccursion(n, days, cost, i);
+
+    // move index according to 30 daya pass
+    for (i = index; i < n && days[i] < days[index] + 30; i++);
+
+    int month_pass = cost[2] + reccursion(n, days, cost, i);
+
+    return min(day_pass, min(week_pass, month_pass));
+}
+
+int memoization(int n, vector<int> &days, vector<int> &cost, int index, vector<int> &dp) {
+
+    if (index >= n) {
+        return 0;
+    }
+
+    if (dp[index] != -1) return dp[index];
+
+    int day_pass = cost[0] + memoization(n, days, cost, index+1, dp);
+
+    // move your index at correct position according to 7 days pass
+    int i;
+    for (i = index; i < n && days[i] < days[index] + 7; i++);
+
+    int week_pass = cost[1] + memoization(n, days, cost, i, dp);
+
+    // move index according to 30 daya pass
+    for (i = index; i < n && days[i] < days[index] + 30; i++);
+
+    int month_pass = cost[2] + memoization(n, days, cost, i, dp);
+
+    return dp[index] = min(day_pass, min(week_pass, month_pass));
+}
+
+int tabulation(int n, vector<int> &days, vector<int> &cost) {
+
+    vector<int> dp(n+1, INT_MAX);
+    dp[n] = 0;
+
+    for (int k = n-1; k >= 0; k--) {
+        int day_pass = cost[0] + dp[k+1];
+
+        // move your index at correct position according to 7 days pass
+        int i;
+        for (i = k; i < n && days[i] < days[k] + 7; i++);
+
+        int week_pass = cost[1] + dp[i];
+
+        // move index according to 30 daya pass
+        for (i = k; i < n && days[i] < days[k] + 30; i++);
+
+        int month_pass = cost[2] + dp[i];
+
+        dp[k] = min(day_pass, min(week_pass, month_pass));
+    }
+
+    return dp[0];
+}
+
+int minimumCoins(int n, vector<int> days, vector<int> cost)
+{
+    // return reccursion(n, days, cost, 0);
+
+    return tabulation(n, days, cost);
+}
