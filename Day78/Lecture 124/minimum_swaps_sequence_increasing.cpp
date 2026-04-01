ceb9e9d7be5ec4d7d657d70436178495c7112cd1
Axit Tiwari <axittiwari0@gmail.com>
2026-04-01 19:54:27 +0530

    added all the optimised solution for minimum swap question

--- Day78/Lecture 124/minimum_swaps_sequence_increasing.cpp
+++ Day78/Lecture 124/minimum_swaps_sequence_increasing.cpp
@@ -1,6 +1,84 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Tabulation {
+public:
+    int tabulation(vector<int>& nums1, vector<int>& nums2) {
+
+        vector<vector<int>> dp(nums1.size()+1, vector<int> (2, 0));
+
+        for (int index = nums1.size()-1; index > 0; index--) {
+
+            for (int swaped = 0; swaped <= 1; swaped++) {
+
+                int min_operations = INT_MAX;
+
+                // *** get previous element of both vectors ***
+                int prev1 = nums1[index-1];
+                int prev2 = nums2[index-1];
+
+                if (swaped) {
+                    swap(prev1, prev2);
+                }
+
+                if (nums1[index] > prev1 && nums2[index] > prev2) {
+                    min_operations = dp[index+1][0];
+                }
+
+                // case 2: you swapped the element of both vector
+                if (nums1[index] > prev2 && nums2[index] > prev1) {
+                    min_operations = min(min_operations, 1 + dp[index+1][1]);
+                }
+
+                dp[index][swaped] = min_operations;
+            }
+        }
+
+        return dp[1][0];
+    }
+
+    int space_optimised(vector<int>& nums1, vector<int>& nums2) {
+
+        int swap_ = 0, no_swap = 0;
+
+        int cur_swap = 0, cur_no_swap = 0;
+
+        for (int index = nums1.size()-1; index > 0; index--) {
+
+            for (int swaped = 0; swaped <= 1; swaped++) {
+
+                int min_operations = INT_MAX;
+
+                // *** get previous element of both vectors ***
+                int prev1 = nums1[index-1];
+                int prev2 = nums2[index-1];
+
+                if (swaped) {
+                    swap(prev1, prev2);
+                }
+
+                if (nums1[index] > prev1 && nums2[index] > prev2) {
+                    min_operations = no_swap;
+                }
+
+                // case 2: you swapped the element of both vector
+                if (nums1[index] > prev2 && nums2[index] > prev1) {
+                    min_operations = min(min_operations, 1 + swap_);
+                }
+
+                if (swaped) cur_swap = min_operations;
+                else {
+                    cur_no_swap = min_operations;
+                }
+            }
+            swap_ = cur_swap;
+            no_swap = cur_no_swap;
+        }
+
+        return min(swap_, no_swap);
+    }
+};
+
 class Solution {
 public:
     int recursion(vector<int>& nums1, vector<int>& nums2, int index, bool swapped) {
