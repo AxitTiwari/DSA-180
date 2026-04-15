c3d5baa63c250ddd75c31a8116a4ef95beee9e6c
Axit Tiwari <axittiwari0@gmail.com>
2026-04-15 13:18:09 +0530

    insert interval (57)

---
+++ Day92/leetcode/insert_interval.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/insert-interval/submissions/1978979848
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> ans;
+
+        int i = 0;
+
+        // left
+        while (i < intervals.size() && intervals[i][1] < newInterval[0]) {
+            ans.push_back({intervals[i][0], intervals[i][1]});
+            i++;
+        }
+
+        // overlapping
+        while (i < intervals.size() && intervals[i][0] <= newInterval[1]) {
+
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i += 1;
+        }
+        ans.push_back(newInterval);
+
+        // right
+        while (i < intervals.size()) {
+            ans.push_back(intervals[i]);
+            i += 1;
+        }
+        return ans;
+    }
+};
