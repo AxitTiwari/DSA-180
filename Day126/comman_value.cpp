a0e4d74a353e7944f73d72ec68698c4effc3e1a2
Axit Tiwari <axittiwari0@gmail.com>
2026-05-20 09:28:25 +0530

    minimum common value

---
+++ Day126/comman_value.cpp
@@ -0,0 +1,24 @@
+// https://leetcode.com/submissions/detail/2006860443/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        
+        for (int i = 0, j = 0; i < nums1.size() && j < nums2.size(); ) {
+
+            if (nums1[i] == nums2[j]) {
+                return nums1[i];
+            }
+            else if (nums1[i] > nums2[j]) {
+                j++;
+            }
+            else {
+                i++;
+            }
+        }
+        return -1;
+    }
+};
