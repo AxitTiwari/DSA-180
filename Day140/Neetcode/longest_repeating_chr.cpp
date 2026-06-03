b1ba995f45b2b29eff495ddf9e22cd0c1ad2fdee
Axit Tiwari <axittiwari0@gmail.com>
2026-06-03 09:08:49 +0530

    added code for day 2 of neetcode

---
+++ Day140/Neetcode/longest_repeating_chr.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/longest-repeating-character-replacement/description/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        
+        int max_size = 0;
+        unordered_map<char, int> count;
+
+        int l = 0;
+        for (int r = 0; r < s.length(); r++) {
+
+            count[s[r]]++;
+
+            int max_count_in_window = 0;
+            for (char i = 'A'; i <= 'Z'; i++) {
+                max_count_in_window = max(max_count_in_window, count[i]);
+            }
+
+            if ((r - l + 1) - max_count_in_window > k) {
+                count[s[l]]--;
+                l++;
+            }
+
+            max_size = max(max_size, r - l + 1);
+        }
+        return max_size;
+    }
+};
---
+++ Day140/Neetcode/longest_substr.cpp
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/2019750301
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+        int max_len = 0;
+
+        unordered_map<char, int> mp;
+        
+        int left = 0;
+        for (int right = 0; right < s.length(); right++) {
+
+            if (mp.count(s[right])) {
+
+                // ##imp
+                left = max(left, mp[s[right]] + 1);
+
+            }
+
+            mp[s[right]] = right;
+            max_len = max(max_len, right - left + 1);
+        }
+        return max_len;
+    }
+};
