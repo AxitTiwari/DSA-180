403ac55f90a9108941d7007cc533ec0842d9b3e1
Axit Tiwari <axittiwari0@gmail.com>
2026-06-20 20:29:57 +0530

    added question of Day157

---
+++ Day157/remove_duplicates.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/submissions/detail/2038716348/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string st;
+
+        for (char ch : s) {
+            if (!st.empty() && st.back() == ch) {
+                st.pop_back();
+            } else {
+                st.push_back(ch);
+            }
+        }
+
+        return st;
+    }
+};
