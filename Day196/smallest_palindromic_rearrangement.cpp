28141cfa68b4ae740b384dacf2b337ba9c7d8d97
Axit Tiwari <axittiwari0@gmail.com>
2026-07-29 00:05:36 +0530

    smallest palindromic rearrangement I

---
+++ Day196/smallest_palindromic_rearrangement.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/smallest-palindromic-rearrangement-i/submissions/2085222669
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        
+        vector<int> freq(26, 0);
+        for (char c : s) freq[c-'a']++;
+
+        string first = "";
+        string middle = "";
+        for (int i = 0; i < 26; i++) {
+
+            first += string(freq[i]/2, char('a'+i));
+
+            if (freq[i] % 2) middle = char('a' + i);
+        }
+
+        string second = first;
+        reverse(second.begin(), second.end());
+        return first + middle + second;
+    }
+};
