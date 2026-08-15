67478c53353f170a0fa39401cda097bb28026ca7
Axit Tiwari <axittiwari0@gmail.com>
2026-08-15 12:03:51 +0530

    all funll BT (894)

---
+++ Day213/all_full_BT.cpp
@@ -0,0 +1,52 @@
+// https://leetcode.com/problems/all-possible-full-binary-trees/submissions/2106472622
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<TreeNode*> allPossibleFBT(int n) {
+        
+        return solve(n);
+    }
+
+private:
+    vector<TreeNode*> solve(int n) {
+
+        if (n % 2 == 0) return {};
+
+        if (n == 1) {
+            TreeNode* node = new TreeNode(0);
+            return {node};
+        }
+
+        vector<TreeNode*> result;
+
+        for (int i = 1; i < n; i += 2) {
+
+            vector<TreeNode*> left = solve(i);
+            vector<TreeNode*> right = solve(n-i-1);
+
+            for (auto l : left) {
+                for (auto r : right) {
+
+                    TreeNode* root = new TreeNode(0);
+                    root->left = l;
+                    root->right = r;
+
+                    result.push_back(root);
+                }
+            }
+        }
+        return result;
+    }
+};
