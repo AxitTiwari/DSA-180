d4b984460392abf46fc00041c8c1cdab1192b463
Axit Tiwari <axittiwari0@gmail.com>
2026-08-26 22:30:45 +0530

    avg of level in binary tree

---
+++ Day225/avg_lvl_bt.cpp
@@ -0,0 +1,44 @@
+// https://leetcode.com/problems/average-of-levels-in-binary-tree/submissions/2121057597
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        
+        vector<double> res;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+
+            int size = q.size();
+
+            double avg = 0;
+            for (int i = 0; i < size; i++) {
+
+                TreeNode* node = q.front();
+                q.pop();
+
+                avg += node->val;
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+
+            res.push_back(avg/size);
+        }
+        return res;
+    }
+};
